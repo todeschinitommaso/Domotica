0a6db8946d47abf968c63b9642dcad1b271bc058
Todeschini.19331 <Todeschini.19331@LABI1-6PC03>
2022-10-24 08:14:44 +0200

    Tv

---
+++ Telecomando/Televisione.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telecomando
+{
+    internal class Televisione
+    {
+        public bool stato;
+
+        public int volume;
+
+        public int canale;
+
+        public string produttore;
+
+        public string modello;
+
+        public int setCanale(int nuovoCanale)
+        {
+            canale = nuovoCanale;
+            return canale;
+        }
+
+        public int setVolume(int nuovoVolume)
+        {
+            volume = nuovoVolume;
+            return volume;
+        }
+
+        public void accendi()
+        {
+            setStato(true);
+        }
+        public void spegni()
+        {
+            setStato(false);
+        }
+
+        private void setStato(bool nuovoStato)
+        {
+            stato = nuovoStato;
+        }
+    }
+}
